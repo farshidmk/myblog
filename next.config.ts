11ae304b5188644ca8bc6a1134c3804e15482e51
manoochehri <sadra@sadra.com>
2025-02-26 14:49:56 +0330

    remove github pages config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  reactStrictMode: true,
-  basePath: "/myblog",
 };
 
 export default nextConfig;
